8ac2244221e9fd732f209fe230e4f6d6c5a51a02
gabriel6181997 <gabriel6181997@gmail.com>
2021-08-21 18:44:06 +0800

    ♻️ Use object property shortcuts and object destructuring

--- utils/forecast.js
+++ utils/forecast.js
@@ -1,15 +1,18 @@
 const request = require("postman-request");
 
 const forecast = (latitude, longitude, callback) => {
-  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=9cc1cac8f17bae8092273b9eed540834`
+  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=9cc1cac8f17bae8092273b9eed540834`;
 
-  request({ url: url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, { body } = {}) => {
     if (error) {
       callback("Unable to connect to weather forecast services!", undefined);
-    } else if (response.body.error) {
+    } else if (body.error) {
       callback("Unable to find location. Try another search", undefined);
     } else {
-      callback(undefined, `${response.body.weather[0].description}.It is current ${response.body.main.temp} degrees out. It feels like ${response.body.main.feels_like} degrees out.`);
+      callback(
+        undefined,
+        `${body.weather[0].description}.It is current ${body.main.temp} degrees out. It feels like ${body.main.feels_like} degrees out.`
+      );
     }
   });
 };
